684108486d6fdf1c1f94211e64c33edcdd5294a0
Max <max@depf.net>
2015-12-31 14:25:46 +0100

    Cleanup

--- converter.sh
+++ converter.sh
@@ -22,42 +22,40 @@ channels=$3
 language=$4
 audiocounter=$5
 subtitlecounter=$6
-                                case $codectype in
-                                        "audio")
-                                                case $codecname in
-                                                        "mp3")
-                                                                echo copying $codecname audio track with $channels channels >&2
-                                                                echo -map 0:a:$audiocounter -metadata:s:a:$audiocounter language=$language -c:a:$audiocounter copy
-                                                                ;;
-                                                        "ac3")
-                                                                if [ $channels != "6" ]
-                                                                then
-                                                                        echo encoding $codecname audio track with $channels channels to 192k mp3 >&2
-                                                                        echo -map 0:a:$audiocounter -metadata:s:a:$audiocounter language=$language -c:a:$audiocounter libmp3lame -b:a:$audiocounter 192k
-                                                                else
-                                                                        echo copying $codecname audio track with $channels channels >&2
-                                                                        echo -map 0:a:$audiocounter -metadata:s:a:$audiocounter language=$language -c:a:$audiocounter copy
-                                                                fi
-                                                                ;;
-                                                        *)
-                                                                if [ $channels != "6" ]
-                                                                then
-                                                                        echo encoding $codecname audio track with $channels channels to 192k mp3 >&2
-                                                                        echo -map 0:a:$audiocounter -metadata:s:a:$audiocounter language=$language -c:a:$audiocounter libmp3lame -b:a:$audiocounter 192k
-                                                                else
-                                                                        echo encoding $codecname audio track with $channels channels to 640k ac3 >&2
-                                                                        echo -map 0:a:$audiocounter -metadata:s:a:$audiocounter language=$language -c:a:$audiocounter ac3 -b:a:$audiocounter 640k
-                                                                fi
-                                                                ;;
-
-
-                                                esac
-                                        ;;
-					"subtitle")
-						echo copying $codecname subtitle >&2
-						echo -map 0:s:$subtitlecounter -metadata:s:s:$subtitlecounter language=$language -c:s:$subtitlecounter copy
-						;;
-                                esac
+	case $codectype in
+		"audio")
+                        case $codecname in
+	                        "mp3")
+                                        echo copying $codecname audio track with $channels channels >&2
+                                        echo -map 0:a:$audiocounter -metadata:s:a:$audiocounter language=$language -c:a:$audiocounter copy
+                                ;;
+                                "ac3")
+                                        if [ $channels != "6" ]
+                                        then
+                                                echo encoding $codecname audio track with $channels channels to 192k mp3 >&2
+                                                echo -map 0:a:$audiocounter -metadata:s:a:$audiocounter language=$language -c:a:$audiocounter libmp3lame -b:a:$audiocounter 192k
+                                        else
+                                                echo copying $codecname audio track with $channels channels >&2
+                                                echo -map 0:a:$audiocounter -metadata:s:a:$audiocounter language=$language -c:a:$audiocounter copy
+                                        fi
+	                        ;;
+        	                *)
+                                        if [ $channels != "6" ]
+                                        then
+                                                echo encoding $codecname audio track with $channels channels to 192k mp3 >&2
+                                                echo -map 0:a:$audiocounter -metadata:s:a:$audiocounter language=$language -c:a:$audiocounter libmp3lame -b:a:$audiocounter 192k
+                                        else
+                                                echo encoding $codecname audio track with $channels channels to 640k ac3 >&2
+                                                echo -map 0:a:$audiocounter -metadata:s:a:$audiocounter language=$language -c:a:$audiocounter ac3 -b:a:$audiocounter 640k
+                                        fi
+                                ;;
+                        esac
+                        ;;
+		"subtitle")
+			echo copying $codecname subtitle >&2
+			echo -map 0:s:$subtitlecounter -metadata:s:s:$subtitlecounter language=$language -c:s:$subtitlecounter copy
+		;;
+        esac
 
 }
 
@@ -79,59 +77,59 @@ do
     width=1280
     ffprobe -v error -of default=noprint_wrappers=1 -show_entries "stream=index,channels,codec_type,codec_name,width : stream_tags=language" "$path" < /dev/null | ( while read a;
 	do
-	 	if [ $state -eq 0 ]
-			then
-				case $(echo $a | cut -d"=" -f1) in
-					"codec_name")
-						codecname=$(echo $a | cut -d"=" -f2)
-						;;
-					"codec_type")
-						codectype=$(echo $a | cut -d"=" -f2)
-						;;
-					"channels")
-						channels=$(echo $a | cut -d"=" -f2)
-                                                ;;
-					"width")
-						if [ $codectype == "video" ]
-							then
-								width=$(echo $a | cut -d"=" -f2)
-							fi
-						;;
-					"TAG:language"|"TAG:LANGUAGE")
-						language=$(echo $a | cut -d"=" -f2)
-						;;
-					"index")
-						if [ $codecname != "-2" ]
-						then
-							state=1
-						fi
-						;;
+		if [ $state -eq 0 ]
+		then
+			case $(echo $a | cut -d"=" -f1) in
+				"codec_name")
+					codecname=$(echo $a | cut -d"=" -f2)
+				;;
+				"codec_type")
+					codectype=$(echo $a | cut -d"=" -f2)
+				;;
+				"channels")
+					channels=$(echo $a | cut -d"=" -f2)
+                                ;;
+				"width")
+					if [ $codectype == "video" ]
+					then
+						width=$(echo $a | cut -d"=" -f2)
+					fi
+				;;
+				"TAG:language"|"TAG:LANGUAGE")
+					language=$(echo $a | cut -d"=" -f2)
+				;;
+				"index")
+					if [ $codecname != "-2" ]
+					then
+						state=1
+					fi
+				;;
 				esac
 		fi
 		if [ $state -eq 1 ]
-			then
-				ffmpegcommand="$ffmpegcommand $(getStreamLine $codectype $codecname $channels $language $audiocounter $subtitlecounter)"
-				case $codectype in
-					"audio")
-						audiocounter=$(($audiocounter + 1))
-						;;
-                                       	"subtitle")
-                                                subtitlecounter=$(($subtitlecounter + 1))
-                                                ;;
-				esac
-				state=0
-			fi
+		then
+			ffmpegcommand="$ffmpegcommand $(getStreamLine $codectype $codecname $channels $language $audiocounter $subtitlecounter)"
+			case $codectype in
+				"audio")
+					audiocounter=$(($audiocounter + 1))
+				;;
+                               	"subtitle")
+                                        subtitlecounter=$(($subtitlecounter + 1))
+                                ;;
+			esac
+			state=0
+		fi
 	done
 	ffmpegcommand="$ffmpegcommand $(getStreamLine $codectype $codecname $channels $language $audiocounter $subtitlecounter)"
 	metadatamodifier="-map_metadata -1 -map_chapters 0"
 	if [ $width -gt 1280 ]
-		then
-		        echo ffmpeg -i "$path" $metadatamodifier -metadata title="${filename%.*}" -map 0:v:0 -vf "scale='min(iw,1280)':'trunc(ow/a/2)*2'" -c:v:0 libx264 -preset medium -crf 22 $ffmpegcommand "${outputhpath}$filename" 
-		        ffmpeg -i "$path" $metadatamodifier -metadata title="${filename%.*}" -map 0:v:0 -vf "scale='min(iw,1280)':'trunc(ow/a/2)*2'" -c:v:0 libx264 -preset medium -crf 22 $ffmpegcommand "${outputpath}$filename" </dev/null 
-		else
-                        echo ffmpeg -i "$path" $metadatamodifier -metadata title="${filename%.*}" -map 0:v:0 -c:v:0 libx264 -preset medium -crf 22 $ffmpegcommand "${outputhpath}$filename" 
-                        ffmpeg -i "$path" $metadatamodifier -metadata title="${filename%.*}" -map 0:v:0 -c:v:0 libx264 -preset medium -crf 22 $ffmpegcommand "${outputpath}$filename" </dev/null 
-		fi )
+	then
+	        echo ffmpeg -i "$path" $metadatamodifier -metadata title="${filename%.*}" -map 0:v:0 -vf "scale='min(iw,1280)':'trunc(ow/a/2)*2'" -c:v:0 libx264 -preset medium -crf 22 $ffmpegcommand "${outputhpath}$filename" 
+	        ffmpeg -i "$path" $metadatamodifier -metadata title="${filename%.*}" -map 0:v:0 -vf "scale='min(iw,1280)':'trunc(ow/a/2)*2'" -c:v:0 libx264 -preset medium -crf 22 $ffmpegcommand "${outputpath}$filename" </dev/null 
+	else
+                echo ffmpeg -i "$path" $metadatamodifier -metadata title="${filename%.*}" -map 0:v:0 -c:v:0 libx264 -preset medium -crf 22 $ffmpegcommand "${outputhpath}$filename" 
+                ffmpeg -i "$path" $metadatamodifier -metadata title="${filename%.*}" -map 0:v:0 -c:v:0 libx264 -preset medium -crf 22 $ffmpegcommand "${outputpath}$filename" </dev/null 
+	fi )
 
 done
 
